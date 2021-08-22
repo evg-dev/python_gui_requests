6a243b40793cdfacd550f38625ebf8669395eba2
Evgeny <evg.develop@gmail.com>
2021-08-22 23:56:24 +0500

    add output_status_code, gui labels

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import sys
 
 import requests as r
 from PyQt5.QtGui import QFont, QIntValidator
+from PyQt5 import QtWidgets, QtCore
 from PyQt5.QtWidgets import QLabel, QApplication, QAction, QSlider, QFileDialog, QMainWindow, QPushButton, qApp, \
     QInputDialog, QLineEdit, QComboBox, QTextEdit
 
@@ -11,19 +12,28 @@ from PyQt5.QtWidgets import QLabel, QApplication, QAction, QSlider, QFileDialog,
 # from PyQt5.QtCore import QThread, Qt, pyqtSignal, pyqtSlot
 # from PyQt5.QtGui import QPixmap, QImage
 
+def on_changed(text):
+    print(text)
+
+
 class MainWindow(QMainWindow):
-    default_url = 'https://evg-dev.github.io/'
+    default_url = 'http://example.com/'
 
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
 
         self.setWindowTitle('Python Gui Requests')
-        self.setGeometry(300, 300, 640, 900)
-        self.setFixedSize(640, 900)
-        # create a label
-        self.label = QLabel(self)
-        # self.label.move(0, 20)
-        # self.label.resize(640, 480)
+        self.setGeometry(300, 300, 640, 1100)
+        self.setFixedSize(640, 1100)
+        # self.setFixedSize(self.sizeHint())
+        # create a labels
+        self.label_headers = QLabel('Response Headers', self)
+        self.label_headers.setGeometry(0, 100, 640, 70)
+        self.label_headers.setAlignment(QtCore.Qt.AlignCenter)
+
+        self.label_body = QLabel('Body response', self)
+        self.label_body.setGeometry(0, 100, 640, 950)
+        self.label_body.setAlignment(QtCore.Qt.AlignCenter)
 
         self.method = QComboBox(self)
 
@@ -37,28 +47,29 @@ class MainWindow(QMainWindow):
 
         self.method.move(50, 50)
         self.method.resize(100, 30)
-        self.method.activated[str].connect(self.on_changed)
+        self.method.activated[str].connect(on_changed)
 
         self.url = QLineEdit(self)
         self.url.move(150, 50)
         self.url.resize(400, 30)
         self.url.setText(self.default_url)
-        # self.method.setMaxLength(40)
-
-        # self.method.setAlignment(Qt.AlignRight)
-        # self.method.setFont(QFont("Arial", 20))
 
         self.output_headers = QTextEdit(self)
         self.output_headers.setReadOnly(True)
         self.output_headers.setLineWrapMode(QTextEdit.NoWrap)
-        self.output_headers.move(0, 100)
+        self.output_headers.move(0, 150)
         self.output_headers.resize(640, 400)
 
         self.output_body = QTextEdit(self)
         self.output_body.setReadOnly(True)
         self.output_body.setLineWrapMode(QTextEdit.NoWrap)
-        self.output_body.move(0, 500)
+        self.output_body.move(0, 600)
         self.output_body.resize(640, 400)
+
+        self.output_status_code = QLabel('Status code : ', self)
+        self.output_status_code.move(0, 70)
+        self.output_status_code.setGeometry(0, 70, 640, 70)
+        self.output_status_code.setAlignment(QtCore.Qt.AlignCenter)
 
         self.send_request_btn = QPushButton('Send Request', self)
         self.send_request_btn.move(500, 50)
@@ -71,7 +82,7 @@ class MainWindow(QMainWindow):
 
         method = self.method.currentText()
         url = self.url.text()
-
+        # TODO: set headers, and post params
         headers = {}
         # params.url = url
         response = ''
@@ -97,23 +108,14 @@ class MainWindow(QMainWindow):
         if method == 'OPTIONS':
             response = r.options(url)
 
-        # h = response.headers
-        # h = str(h)
-        # h = json.loads(h)
-        # h = {"name": "Gilbert", "wins": [["straight", "7"], ["one pair", "10"]]}
-        # j = json.dumps(h, sort_keys=True, indent=4)
-        # print(h)
-        # self.output_headers.setPlainText((str(response.headers)))
+        self.get_response(response)
 
+    def get_response(self, response):
         h = json.dumps(dict(response.headers), sort_keys=True, indent=4)
+        self.output_status_code.setText(
+            'Status code : ' + '<span style="font-weight:600;">' + str(response.status_code) + '</span>')
         self.output_headers.setPlainText(h)
-
         self.output_body.setPlainText(response.text)
-
-    def on_changed(self, text):
-        print(text)
-        # self.qlabel.setText(text)
-        # self.qlabel.adjustSize()
 
 
 if __name__ == '__main__':
